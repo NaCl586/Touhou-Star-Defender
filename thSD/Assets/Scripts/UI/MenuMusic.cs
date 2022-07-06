027c76438a456268fec49c9d661825e83bb72e7b
NaCl586 <73216718+NaCl586@users.noreply.github.com>
2022-07-06 12:42:11 +0700

    final revision

---
+++ thSD/Assets/Scripts/UI/MenuMusic.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusic : MonoBehaviour
+{
+    void Awake()
+    {
+        this.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("musicVolume", 0.25f);
+    }
+}
